4752f10e7b0800296a2f433db95fb43e6d245691
pranong <nutpat2539@gmail.com>
2025-04-26 21:55:50 +0700

    change to ip

--- taladpla-web/src/app/service/search-doc/search-doc.service.ts
+++ taladpla-web/src/app/service/search-doc/search-doc.service.ts
@@ -10,7 +10,7 @@ export class SearchDocService {
     return this.http.get(`${this.baseUrl}/users`);
   }
 
-  private baseUrl = 'http://backend:3000/api';
+  private baseUrl = 'http://139.59.219.248:3000/api';
 
   constructor(private http: HttpClient) { }
 
