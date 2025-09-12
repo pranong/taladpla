b64d2a132bf8efd9b7baa39ae7f492b681f4a804
pranong <nutpat2539@gmail.com>
2025-09-12 22:30:02 +0700

    env

--- taladpla-web/src/environments/environment.prod.ts
+++ taladpla-web/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://139.59.219.248:3000/api',
+  // apiUrl: 'http://139.59.219.248:3000/api',
+  apiUrl: 'http://localhost:3000/api',
 };
