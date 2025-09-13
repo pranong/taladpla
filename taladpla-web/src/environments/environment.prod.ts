0cac6ed97ae2b6c01008fc3b5441dfd20992e4e0
pranong <nutpat2539@gmail.com>
2025-09-14 01:04:41 +0700

    proxy on

--- taladpla-web/src/environments/environment.prod.ts
+++ taladpla-web/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://139.59.219.248:3000/api',
-  apiUrl: 'http://pnkrwk.thddns.net:7661/api',
+  // apiUrl: 'http://pnkrwk.thddns.net:7661/api',
+  apiUrl: 'http://pnkrwk.thddns.net/api',
 };
